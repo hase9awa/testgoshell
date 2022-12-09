907dfb0851fbfcc193961828740c26cee561208c
VasilyLomagin <nerzulov@gmail.com>
2022-12-09 15:40:53 +0400

    initial commit

---
+++ testgo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for ((i=1; i < 101; i++))
+do
+  if [[ $((i % 15)) -eq 0 ]]; then
+    echo -n "FizzBuzz,"
+  elif [[ $((i % 5)) -eq 0 ]]; then
+    echo -n "Buzz,"
+  elif [[ $((i % 3)) -eq 0 ]]; then
+    echo -n "Fizz,"
+  else
+    echo -n $i,
+  fi
+done
